38d661aef518aca73458620a34030e256ed6bbcc
louis2580 <louis0710@live.fr>
2018-03-03 02:10:03 -0500

    Interaction et dialog avec plusieurs fermiers

--- Assets/Dialog/Scripts/TextBoxManager.cs
+++ Assets/Dialog/Scripts/TextBoxManager.cs
@@ -15,6 +15,7 @@ public class TextBoxManager : MonoBehaviour {
     public int endAtLine = 0;
 
     public GameObject farmer;
+    public Color textColor ;
 
 	// Use this for initialization
 	void Start () {
@@ -31,21 +32,58 @@ public class TextBoxManager : MonoBehaviour {
         {
             endAtLine = textLines.Length;
         }
+
+        if (farmer.name == "Farmer1")
+        {
+            textColor = new Color(255, 255, 0);
+        } else
+        {
+            textColor = new Color(0, 255, 255);
+        }
 	}
 
     void Update()
     {
-        if (Input.GetButtonUp("B") )
+        // Si le joueur clique sur B à côté du farmer ou qu'il avait déjà engagé le dialogue
+        if (Input.GetButtonUp("B") && (farmer.GetComponent<FarmerDialogManager>().waitForDialog || textBox.active))
         {
+            // Si le joueur ne discutait pas déjà avec l'autre farmer
+            if (textBox.active && (currentLine == 0 || textLines[currentLine - 1].Contains(";")))
+                return;
+
+            // Si le joueur à déjà une mission en cours
+            if (farmer.GetComponent<FarmerDialogManager>().isOnMission)
+            {
+                AskForSheep();
+            }
             NextLine();
         }
+    }
+
+    // Si il est en mission et que le joeur reparle au fermier, il répète sa dernière phrase
+    public void AskForSheep()
+    {
+        currentLine--;
+        text.text = textLines[currentLine];
+        text.color = textColor;
+        if (textLines[currentLine].Contains(";"))
+        {
+            textBox.SetActive(false);
+        }
+        else
+        {
+            textBox.SetActive(true);
+        }
+        currentLine++;
     }
 
     public void NextLine()
     {
         text.text = textLines[currentLine];
+        text.color = textColor;
         if (textLines[currentLine].Contains(";"))
         {
+            farmer.GetComponent<FarmerDialogManager>().BeginMission();
             textBox.SetActive(false);
         } else
         {
