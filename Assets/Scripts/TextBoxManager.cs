2f9821da3ebca120283322ab8103fddfc0831c66
louis2580 <louis0710@live.fr>
2018-03-02 23:55:12 -0500

    debut fenetre dialog

---
+++ Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+
+    public GameObject textBox;
+    public Text text;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine = 0;
+    public int endAtLine = 0;
+
+    public GameObject farmer;
+
+	// Use this for initialization
+	void Start () {
+
+        textBox.SetActive(false);
+        
+		if (textFile != null)
+        {
+            Debug.Log(textFile.text);
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if (endAtLine == 0)
+        {
+            endAtLine = textLines.Length;
+        }
+	}
+
+    void Update()
+    {
+        
+    }
+
+    public void NextLine()
+    {
+        text.text = textLines[currentLine];
+
+        if (textBox.active == false)
+        {
+            textBox.SetActive(true);
+        }
+        if (textLines[currentLine].Length == 0)
+        {
+            textBox.SetActive(false);
+        }
+
+    }
+
+   
+
+}
