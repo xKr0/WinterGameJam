7cd7d5e801002ec4194c5fb66005255737b63daf
sebspas <sebspas@gmail.com>
2018-03-02 20:25:30 -0500

    ColorManager basic

--- Assets/Color/ColorManager.cs
+++ Assets/Color/ColorManager.cs
@@ -17,6 +17,25 @@ public class ColorManager
         Magenta
     }
 
-    Dictionary<ColorList, >
+    Dictionary<ColorList, Color> ColorEnumToColor = new Dictionary<ColorList, Color>();
+
+    private static ColorManager instance;
+
+    private ColorManager()
+    {
+        ColorEnumToColor.Add(ColorList.Red, new Color(255, 0, 0));
+    }
+
+    public static ColorManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new ColorManager();
+            }
+            return instance;
+        }
+    }
 
 }
