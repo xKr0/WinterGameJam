3e326a08409b73986639b6e015ca00859603c914
sebspas <sebspas@gmail.com>
2018-03-04 12:32:57 -0500

    Debug other

--- Assets/Map/Script/MenuHandler.cs
+++ Assets/Map/Script/MenuHandler.cs
@@ -30,7 +30,7 @@ public class MenuHandler : MonoBehaviour {
 	}
 
     public void Play(){
-        Application.LoadLevel(scene);
+        SceneManager.LoadScene(scene);
     }
 
     public void Credits(){
