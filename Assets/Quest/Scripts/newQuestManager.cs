b9866edc3d324ec8235bac6af8418bea9a8c4d22
xkr0 <cros.camille31@gmail.com>
2018-03-03 19:19:52 -0500

    new quest manager v1

---
+++ Assets/Quest/Scripts/newQuestManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newQuestManager : MonoBehaviour 
+{
+    private Quest currentQuest = null;
+    private Collider currentClient = null;
+    private bool isOnMission = false;
+
+    private float timer = 0.0f;
+
+    public void ActivateQuest(Quest quest, Collider client)
+    {
+        currentQuest = quest;
+        currentClient = client;
+        isOnMission = true;
+    }
+
+    void Update()
+    {
+        if (isOnMission)
+        {
+            TickTock();
+        }
+    }
+
+    void TickTock()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= currentQuest.Timer)
+        {
+            FailQuest();
+        }
+    }
+
+    void RegisterClientEvent()
+    {
+        
+    }
+
+    void SucceedQuest()
+    {
+        Debug.Log("Time.out");
+        ResetAll();
+    }
+
+    void FailQuest()
+    {
+        Debug.Log("Time.out");
+        ResetAll();
+    }
+
+    void ResetAll()
+    {
+        isOnMission = false;
+        currentQuest = null;
+        currentClient = null;
+        timer = 0.0f;
+    }
+}
